51b0c627d2613dd700267bfd17226084d5195e77
lilinling12 <66773695+lilinling12@users.noreply.github.com>
2026-08-14 14:43:11 +0800

    feat(security): expose managed subject process context

--- src/avp_ref/security/__init__.py
+++ src/avp_ref/security/__init__.py
@@ -1,10 +1,13 @@
 """Reference Security-profile implementation helpers."""
 
 from .capability_policy import CapabilityDenialRecord, CapabilityGuardPolicy
+from .process_context import ManagedSubjectProcessContext, SubjectProcessContextResult
 from .subject_adapter import CapabilityGuardedSubjectAdapter
 
 __all__ = [
     "CapabilityDenialRecord",
     "CapabilityGuardPolicy",
     "CapabilityGuardedSubjectAdapter",
+    "ManagedSubjectProcessContext",
+    "SubjectProcessContextResult",
 ]
