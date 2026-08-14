e75b1012be6cc6235817b28b5ef024473b2e2cf0
lilinling12 <66773695+lilinling12@users.noreply.github.com>
2026-08-15 04:24:49 +0800

    feat(otel): register reference TCK adapter

--- src/avp_ref/tck_adapter/reference_composite.py
+++ src/avp_ref/tck_adapter/reference_composite.py
@@ -10,6 +10,7 @@ from .reference_aligned import AlignedReferenceTCKAdapter
 from .reference_environment import ReferenceEnvironmentTCKAdapter
 from .reference_evidence import ReferenceEvidenceTCKAdapter
 from .reference_mcp import ReferenceMCPTCKAdapter
+from .reference_opentelemetry import ReferenceOpenTelemetryTCKAdapter
 from .reference_oracle import ReferenceOracleTCKAdapter
 from .reference_scenario import ReferenceScenarioTCKAdapter
 from .reference_security import ReferenceSecurityTCKAdapter
@@ -29,6 +30,7 @@ class ReferenceConformanceAdapter:
             ReferenceScenarioTCKAdapter(),
             ReferenceEnvironmentTCKAdapter(),
             ReferenceMCPTCKAdapter(),
+            ReferenceOpenTelemetryTCKAdapter(),
             ReferenceSecurityTCKAdapter(),
             ReferenceSecurityFaultTCKAdapter(),
             ReferenceSecurityAssuranceTCKAdapter(),
